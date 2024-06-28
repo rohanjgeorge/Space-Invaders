fa57ba6fb859292ed97e7d3a64e7b98d506e08e9
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-06-29 01:54:36 +0530

    Assignment: Create a Game Window

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,30 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "SFML Window");
+
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        // Clear the window
+        window.clear(sf::Color::Green);
+
+        // Display whatever you draw
+        window.display();
+    }
+
     return 0;
 }
