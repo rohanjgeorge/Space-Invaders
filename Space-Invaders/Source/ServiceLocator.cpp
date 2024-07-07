89de80bda86bf08004ad27f5a279cf8c31148058
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-07-08 03:11:32 +0530

    Assignment: Create Service Locator

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,30 @@
+#include "../Header/ServiceLocator.h"
+
+ServiceLocator::~ServiceLocator()
+{
+}
+
+void ServiceLocator::createServices()
+{
+}
+
+void ServiceLocator::clearAllServices()
+{
+}
+
+ServiceLocator* ServiceLocator::getInstance()
+{
+	return nullptr;
+}
+
+void ServiceLocator::initialize()
+{
+}
+
+void ServiceLocator::update()
+{
+}
+
+void ServiceLocator::render()
+{
+}
