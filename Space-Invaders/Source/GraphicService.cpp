b8b233a409d1676c440b06bfda905f5a605c206e
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-07-14 10:29:28 +0530

    Assignment: Recreate the Game Window

---
+++ Space-Invaders/Source/GraphicService.cpp
@@ -0,0 +1,57 @@
+#include "../Header/GraphicService.h"
+
+void GraphicService::setVideoMode()
+{
+    video_mode = new sf::VideoMode(game_window_width, game_window_height, sf::VideoMode::getDesktopMode().bitsPerPixel);
+}
+
+void GraphicService::onDestroy()
+{
+    delete(video_mode);
+    delete(game_window);
+}
+
+GraphicService::GraphicService()
+{
+    game_window = nullptr;
+    video_mode = nullptr;
+}
+
+GraphicService::~GraphicService()
+{
+    onDestroy();
+}
+
+sf::RenderWindow* GraphicService::createGameWindow()
+{
+    setVideoMode();
+    return new sf::RenderWindow(*video_mode, game_window_title);
+}
+
+void GraphicService::initialize()
+{
+    game_window = createGameWindow();
+}
+
+void GraphicService::update()
+{
+}
+
+void GraphicService::render()
+{
+}
+
+bool GraphicService::isGameWindowOpen()
+{
+    return game_window->isOpen();
+}
+
+sf::RenderWindow* GraphicService::getGameWindow()
+{
+    return game_window;
+}
+
+sf::Color GraphicService::getWindowColor()
+{
+    return window_color;
+}
--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -89,14 +89,14 @@ int main()
     return 0;
     */
 
-    GameService game_service; //create object
-    game_service.ignite(); // this will 'ignite' or start the flow of our game
+    GameService* game_service = new GameService();; //create object
+    game_service->ignite(); // this will 'ignite' or start the flow of our game
 
     // while the game is running we will update the game loop
-    while (game_service.isRunning())
+    while (game_service->isRunning())
     {
-        game_service.update();
-        game_service.render();
+        game_service->update();
+        game_service->render();
     }
     return 0;
 }
