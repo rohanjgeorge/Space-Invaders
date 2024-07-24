dc3e2d810c4cebab896ba6ec10e54b9d5f27f484
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-07-24 12:00:46 +0530

    Assignment: Implement the Player Model

---
+++ Space-Invaders/Header/Player/PlayerModel.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+enum class PlayerState //Our Enum
+{
+    ALIVE,
+    DEAD,
+    // we will add more states later
+};
+
+
+class PlayerModel
+{
+private:
+
+    const sf::Vector2f initial_player_position = sf::Vector2f(500.f, 500.f); //new var
+    sf::Vector2f player_position; //new var
+    PlayerState player_state;
+    int player_score;
+
+
+public:
+    const sf::Vector2f left_most_position = sf::Vector2f(50.f, 0.f);
+    const sf::Vector2f right_most_position = sf::Vector2f(700.f, 0.f);
+
+    const float player_movement_speed = 200.0f;
+
+    PlayerModel();
+    ~PlayerModel();
+
+    void initialize();
+
+    void reset(); //new method
+
+
+    //getters and setters
+    sf::Vector2f getPlayerPosition();
+    void setPlayerPosition(sf::Vector2f position);
+
+    int getPlayerScore();
+    void setPlayerScore(int score);
+
+    //new getter and setter
+    PlayerState getPlayerState();
+    void setPlayerState(PlayerState state);
+
+};
