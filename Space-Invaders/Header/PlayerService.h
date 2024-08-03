3c342b2deccf78c31f63b7055ff9acf49b32f81b
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-08-03 10:08:51 +0530

    Assignment: Implement the Player View & Controller

--- Space-Invaders/Header/PlayerService.h
+++ Space-Invaders/Header/PlayerService.h
@@ -1,39 +1,16 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+#include "../../Header/Player/PlayerController.h"
 
 class PlayerService
 {
-
 private:
-
-    int health = 3;
-    sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
-    float movement_speed = 350.0f;
-    int player_score = 0;
-
-    const sf::String player_texture_path = "assets/textures/player_ship.png";
-
-    sf::Texture player_texture;
-    sf::Sprite player_sprite;
-
-    sf::RenderWindow* game_window; //as always
-
-    void initializePlayerSprite();
-    void processPlayerInput();
+	PlayerController* player_controller;
 
 public:
+	PlayerService();
+	~PlayerService();
 
-    PlayerService();
-    ~PlayerService();
-
-    void initialize();
-    void update();
-    void render();
-
-    void moveLeft();
-    void moveRight();
-    int getMoveSpeed();
-    sf::Vector2f getPlayerPosition();
-
+	void initialize();
+	void update();
+	void render();
 };
-
