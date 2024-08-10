98da3285ccf81f63906322e9f38df37845185e32
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-08-10 11:47:34 +0530

    Assignment: Implement Enemy Movement

--- Space-Invaders/Header/Enemy/EnemyController.h
+++ Space-Invaders/Header/Enemy/EnemyController.h
@@ -13,6 +13,11 @@ namespace Enemy
         EnemyView* enemy_view;
         EnemyModel* enemy_model;
 
+        void move();
+        void moveLeft();
+        void moveRight();
+        void moveDown();
+
     public:
         EnemyController();
         ~EnemyController();
--- Space-Invaders/Source/Enemy/EnemyService.cpp
+++ Space-Invaders/Source/Enemy/EnemyService.cpp
@@ -20,6 +20,7 @@ namespace Enemy
 
 	void EnemyService::update()
 	{
+		enemy->update();
 	}
 
 	void EnemyService::render()
