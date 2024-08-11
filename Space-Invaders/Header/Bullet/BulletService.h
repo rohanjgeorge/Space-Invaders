dfe7df09b36cf3151cb265098dac0112b994dbab
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-08-11 18:43:08 +0530

    Assignment: Spawn Bullets

--- Space-Invaders/Header/Bullet/BulletService.h
+++ Space-Invaders/Header/Bullet/BulletService.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <vector>
+#include "SFML/System/Vector2.hpp"
+#include "../../Header/Projectile/IProjectile.h"
+
+namespace Bullet
+{
+	class BulletController;
+	enum class BulletType;
+	enum class MovementDirection;
+
+	class BulletService
+	{
+	private:
+		std::vector<Projectile::IProjectile*> bullet_list;
+
+		BulletController* createBullet(BulletType bullet_type);
+		void destroy();
+
+	public:
+		BulletService();
+		virtual ~BulletService();
+
+		void initialize();
+		void update();
+		void render();
+
+		BulletController* spawnBullet(BulletType bullet_type, sf::Vector2f position, MovementDirection direction);
+		void destroyBullet(BulletController* bullet_controller);
+	};
+}
