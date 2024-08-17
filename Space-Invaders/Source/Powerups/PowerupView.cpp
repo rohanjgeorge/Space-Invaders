32e66a8b25463aef3a165113548df276921f3146
rohanjgeorge <45092466+rohanjgeorge@users.noreply.github.com>
2024-08-17 17:28:20 +0530

    Assignment: Change And Use Image View

--- Space-Invaders/Source/Powerups/PowerupView.cpp
+++ Space-Invaders/Source/Powerups/PowerupView.cpp
@@ -1,75 +1,66 @@
 #include "../../header/Powerups/PowerupView.h"
 #include "../../header/Global/ServiceLocator.h"
 #include "../../header/Global/Config.h"
+#include "../../header/Bullet/BulletConfig.h"
 #include "../../header/Powerups/PowerupController.h"
 #include "../../header/Powerups/PowerupConfig.h"
 
 namespace Powerup
 {
 	using namespace Global;
+	using namespace UI::UIElement;
 
-	PowerupView::PowerupView() {  }
+	PowerupView::PowerupView() { createUIElements(); }
 
-	PowerupView::~PowerupView() { }
+	PowerupView::~PowerupView() { destroy(); }
 
 	void PowerupView::initialize(PowerupController* controller)
 	{
 		powerup_controller = controller;
-		game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
-		initializeImage(powerup_controller->getPowerupType());
+		initializeImage();
 	}
 
-
-	void PowerupView::initializeImage(PowerupType type)
+	void PowerupView::createUIElements()
 	{
-		switch (type)
-		{
-		case::Powerup::PowerupType::TRIPLE_LASER:
-			if (powerup_texture.loadFromFile(Config::tripple_laser_texture_path))
-			{
-				powerup_sprite.setTexture(powerup_texture);
-				scaleImage();
-			}
-			break;
-		case::Powerup::PowerupType::SHIELD:
-			if (powerup_texture.loadFromFile(Config::shield_texture_path))
-			{
-				powerup_sprite.setTexture(powerup_texture);
-				scaleImage();
-			}
-			break;
-		case::Powerup::PowerupType::RAPID_FIRE:
-			if (powerup_texture.loadFromFile(Config::rapid_fire_texture_path))
-			{
-				powerup_sprite.setTexture(powerup_texture);
-				scaleImage();
-			}
-			break;
-		case::Powerup::PowerupType::OUTSCAL_BOMB:
-			if (powerup_texture.loadFromFile(Config::outscal_bomb_texture_path))
-			{
-				powerup_sprite.setTexture(powerup_texture);
-				scaleImage();
-			}
-			break;
-		}
+		powerup_image = new ImageView();
 	}
 
-	void PowerupView::scaleImage()
+	void PowerupView::initializeImage()
 	{
-		powerup_sprite.setScale(
-			static_cast<float>(powerup_sprite_width) / powerup_sprite.getTexture()->getSize().x,
-			static_cast<float>(powerup_sprite_height) / powerup_sprite.getTexture()->getSize().y
-		);
+		powerup_image->initialize(getPowerupTexturePath(), powerup_sprite_width, powerup_sprite_height, powerup_controller->getPowerupPosition());
 	}
 
 	void PowerupView::update()
 	{
-		powerup_sprite.setPosition(powerup_controller->getCollectiblePosition());
+		powerup_image->setPosition(powerup_controller->getPowerupPosition());
+		powerup_image->update();
 	}
 
 	void PowerupView::render()
 	{
-		game_window->draw(powerup_sprite);
+		powerup_image->render();
+	}
+
+	sf::String PowerupView::getPowerupTexturePath()
+	{
+		switch (powerup_controller->getPowerupType())
+		{
+		case::Powerup::PowerupType::SHIELD:
+			return Config::shield_texture_path;
+
+		case::Powerup::PowerupType::TRIPLE_LASER:
+			return Config::tripple_laser_texture_path;
+
+		case::Powerup::PowerupType::RAPID_FIRE:
+			return Config::rapid_fire_texture_path;
+
+		case::Powerup::PowerupType::OUTSCAL_BOMB:
+			return Config::outscal_bomb_texture_path;
+		}
+	}
+
+	void PowerupView::destroy()
+	{
+		delete(powerup_image);
 	}
 }
---
+++ Space-Invaders/Source/UI/UIElement/UIView.cpp
@@ -0,0 +1,36 @@
+#include "../../header/UI/UIElement/UIView.h"
+#include "../../header/Global/ServiceLocator.h"
+#include "../../Header/Graphic/GraphicService.h"
+
+namespace UI
+{
+	namespace UIElement
+	{
+		using namespace Global;
+		using namespace Graphic;
+
+		UIView::UIView() = default;
+
+		UIView::~UIView() = default;
+
+		void UIView::initialize()
+		{
+			game_window = ServiceLocator::getInstance()->getGraphicService()->getGameWindow();
+			ui_state = UIState::VISIBLE;
+		}
+
+		void UIView::update() { }
+
+		void UIView::render() { }
+
+		void UIView::show()
+		{
+			ui_state = UIState::VISIBLE;
+		}
+
+		void UIView::hide()
+		{
+			ui_state = UIState::HIDDEN;
+		}
+	}
+}
